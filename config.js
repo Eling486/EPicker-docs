42968efa102582966a6520d967108469479a1ed9
Eling486 <evanchen486@163.com>
2021-01-25 11:54:25 +0800

    fixed bugs

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const langs = [
     { title: 'English', path: '/home', matchPath: /^\/home/ },
     { title: '简体中文', path: '/zh-Hans/', matchPath: /^\/zh-Hans/ },
@@ -54,64 +53,4 @@ function selector0() {
             readonly: true,
             theme: 'sakura_light'
         });
-=======
-const langs = [
-    { title: 'English', path: '/home', matchPath: /^\/home/ },
-    { title: '简体中文', path: '/zh-Hans/', matchPath: /^\/zh-Hans/ },
-];
-
-docute.init({
-    landing: 'landing.html',
-    title: 'ESelector',
-    repo: 'Eling486/ESelector',
-    'edit-link': 'https://github.com/Eling486/ESelector-docs',
-    nav: {
-        default: [
-            {
-                title: 'Home', path: '/'
-            },
-            {
-                title: 'Docs', path: '/home'
-            },
-            {
-                title: 'Languages', type: 'dropdown', items: langs
-            }
-        ],
-        'zh-Hans': [
-            {
-                title: '首页', path: '/'
-            },
-            {
-                title: '文档', path: '/zh-Hans/'
-            },
-            {
-                title: '选择语言', type: 'dropdown', items: langs
-            }
-        ],
-    },
-    plugins: [
-        docsearch({
-            apiKey: '',
-            indexName: 'eselector',
-            tags: ['english', 'zh-Hans'],
-            url: 'https://eselector.js.org'
-        }),
-        evanyou(),
-    ]
-});
-
-function selector0() {
-    if (!window.es0) {
-        document.querySelector(".input#input").removeAttribute("hidden");
-        window.es0 = new ESelector({
-            container: document.getElementById('eselector'),
-            type: 'calendar',
-            rules: 'all',
-            target: document.getElementById('input'),
-            readonly: true,
-            theme: 'sakura_light'
-        });
-    }
-
->>>>>>> 3258a94dd1ce29bf7158fca3ecf2bec66a032699
 }
